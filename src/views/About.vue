721161890726ed41b8a88df2549b99987f16e7f7
Kai Huang <kai.huang4b6169@gmail.com>
2019-10-28 22:01:44 +0800

    About complete

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,12 +1,129 @@
 <template>
-  <div>
-    <h1>This is an about page</h1>
-  </div>
+  <v-container class="grey lighten-5">
+
+    <v-row justify="center">
+      <v-col md="8">
+        <v-card class="pa-2" tile >
+        <v-tabs vertical>
+        <v-tab
+          v-for="(item, i) in items"
+          :key="i"
+        >
+          <v-icon left v-text="item.icon"></v-icon>
+          <v-list-item-title v-text="item.text"></v-list-item-title>
+        </v-tab>
+
+        <v-tab-item>
+          <v-card flat>
+            <v-card-text>
+              <p>
+                Sed aliquam ultrices mauris. Donec posuere vulputate arcu. Morbi ac felis. Etiam feugiat lorem non metus. Sed a libero.
+              </p>
+
+              <p>
+                Nam ipsum risus, rutrum vitae, vestibulum eu, molestie vel, lacus. Aenean tellus metus, bibendum sed, posuere ac, mattis non, nunc. Aliquam lobortis. Aliquam lobortis. Suspendisse non nisl sit amet velit hendrerit rutrum.
+              </p>
+
+              <p class="mb-0">
+                Phasellus dolor. Fusce neque. Fusce fermentum odio nec arcu. Pellentesque libero tortor, tincidunt et, tincidunt eget, semper nec, quam. Phasellus blandit leo ut odio.
+              </p>
+            </v-card-text>
+          </v-card>
+        </v-tab-item>
+        <v-tab-item>
+          <v-card flat>
+            <v-card-text>
+              <p>
+                Morbi nec metus. Suspendisse faucibus, nunc et pellentesque egestas, lacus ante convallis tellus, vitae iaculis lacus elit id tortor. Sed mollis, eros et ultrices tempus, mauris ipsum aliquam libero, non adipiscing dolor urna a orci. Curabitur ligula sapien, tincidunt non, euismod vitae, posuere imperdiet, leo. Nunc sed turpis.
+              </p>
+
+              <p>
+                Suspendisse feugiat. Suspendisse faucibus, nunc et pellentesque egestas, lacus ante convallis tellus, vitae iaculis lacus elit id tortor. Proin viverra, ligula sit amet ultrices semper, ligula arcu tristique sapien, a accumsan nisi mauris ac eros. In hac habitasse platea dictumst. Fusce ac felis sit amet ligula pharetra condimentum.
+              </p>
+
+              <p>
+                Sed consequat, leo eget bibendum sodales, augue velit cursus nunc, quis gravida magna mi a libero. Nam commodo suscipit quam. In consectetuer turpis ut velit. Sed cursus turpis vitae tortor. Aliquam eu nunc.
+              </p>
+
+              <p>
+                Etiam ut purus mattis mauris sodales aliquam. Ut varius tincidunt libero. Aenean viverra rhoncus pede. Duis leo. Fusce fermentum odio nec arcu.
+              </p>
+
+              <p class="mb-0">
+                Donec venenatis vulputate lorem. Aenean viverra rhoncus pede. In dui magna, posuere eget, vestibulum et, tempor auctor, justo. Fusce commodo aliquam arcu. Suspendisse enim turpis, dictum sed, iaculis a, condimentum nec, nisi.
+              </p>
+            </v-card-text>
+          </v-card>
+        </v-tab-item>
+        <v-tab-item>
+          <v-card flat>
+            <v-card-text>
+              <p>
+                Fusce a quam. Phasellus nec sem in justo pellentesque facilisis. Nam eget dui. Proin viverra, ligula sit amet ultrices semper, ligula arcu tristique sapien, a accumsan nisi mauris ac eros. In dui magna, posuere eget, vestibulum et, tempor auctor, justo.
+              </p>
+
+              <p class="mb-0">
+                Cras sagittis. Phasellus nec sem in justo pellentesque facilisis. Proin sapien ipsum, porta a, auctor quis, euismod ut, mi. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nam at tortor in tellus interdum sagittis.
+              </p>
+            </v-card-text>
+          </v-card>
+        </v-tab-item>
+        </v-tabs>
+      
+        </v-card>
+      </v-col>
+    </v-row>
+
+    <v-row justify="center">
+    
+      <v-col md="2">
+        <v-card class="pa-2" tile >
+          <v-list rounded>
+            <v-list-item-group v-model="item" color="primary">
+              <v-list-item
+                v-for="(item, i) in items"
+                :key="i"
+              >
+                <v-list-item-icon>
+                  <v-icon v-text="item.icon"></v-icon>
+                </v-list-item-icon>
+                <v-list-item-content>
+                  <v-list-item-title v-text="item.text"></v-list-item-title>
+                </v-list-item-content>
+              </v-list-item>
+            </v-list-item-group>
+          </v-list>
+        </v-card>
+      </v-col>
+
+      <v-col md="6">
+        <v-card class="pa-2" tile >
+          <v-card-title class="headline mb-1">院所介紹</v-card-title>
+<p>
+&emsp;本診所管理系統為一 Web Application，目的為提供診所一個簡易、通用的管理介面，
+並開源程式碼、開放擴充，使診所能針對各自領域的不同進行擴充，同時也能兼具通用管
+理的功能。<br><br>
+&emsp;系統開發上，前端介面使用 Vue.js 框架技術實做；後端使用 Python；資料庫使用
+MongoDB 及 FHIR Server；資料的查詢（query）及更新使用 GraphQL 進行。
+</p>
+        </v-card>
+      </v-col>
+    </v-row>
+  </v-container>
 </template>
 
 <script lang="ts">
 import { Vue, Component } from "vue-property-decorator";
 
+
 @Component
-export default class About extends Vue {}
+export default class About extends Vue {
+  items: Array<Object> =  [
+        { text: '院所介紹', icon: 'mdi-hospital-building' },
+        { text: '主治項目', icon: 'mdi-stethoscope' },
+        { text: '醫師介紹', icon: 'mdi-doctor' },
+        { text: '院所位置', icon: 'mdi-map-marker' }
+      ]
+  Introduction: String="123";
+}
 </script>
