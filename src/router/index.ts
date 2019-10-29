7a4fd82de8b09c5471cc8300358d8ce3e04c2ece
Kai Huang <kai.huang4b6169@gmail.com>
2019-10-29 14:02:02 +0800

    lastetnews and registration is completed

--- src/router/index.ts
+++ src/router/index.ts
@@ -16,6 +16,15 @@ const routes = [
         component: Home
       },
       {
+        path: "/lastestNews",
+        name: "lastestNews",
+        // route level code-splitting
+        // this generates a separate chunk (about.[hash].js) for this route
+        // which is lazy-loaded when the route is visited.
+        component: () =>
+          import(/* webpackChunkName: "about" */ "@/views/LastestNews.vue")
+      },
+      {
         path: "/about",
         name: "about",
         // route level code-splitting
