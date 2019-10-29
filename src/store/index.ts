03cebf8ae3b5ae437491483d2df95afb0c7a2efe
Mino <madness48596@gmail.com>
2019-10-29 23:52:16 +0800

    Added User model in Vuex

--- src/store/index.ts
+++ src/store/index.ts
@@ -2,13 +2,14 @@ import Vue from "vue";
 import Vuex, { StoreOptions } from "vuex";
 import { RootState } from "./types";
 import { Dashboard } from "./modules/dashboard/index";
+import { User } from "./modules/user/index";
 
 Vue.use(Vuex);
 
 const store: StoreOptions<RootState> = {
   state: {
-    version: "1.0.0" // a simple property
+    version: "1.0.0"
   },
-  modules: { Dashboard }
+  modules: { Dashboard, User }
 };
 export default new Vuex.Store<RootState>(store);
