fcd35dc754a05fe3f2bb8e813f2416f952ebfe4d
Kai Huang <kai.huang4b6169@gmail.com>
2019-11-05 13:39:43 +0800

    create ragistrationInfo and lastestNews in component

---
+++ src/components/RegistrationInfo.vue
@@ -0,0 +1,40 @@
+<template>
+  <div>
+    <v-card-title class="py-2" color="grey">
+      看診資訊
+    </v-card-title>
+
+    <v-divider class="mx-4"></v-divider>
+
+    <v-row>
+      <v-card-text class="py-2 px-6">
+        看診醫生： {{ registrationInfoContent.doctorName }}
+      </v-card-text>
+    </v-row>
+
+    <v-row>
+      <v-card-text class="py-2 px-6">
+        今日掛號人數： {{ registrationInfoContent.totleRegistration }}
+      </v-card-text>
+    </v-row>
+
+    <v-row>
+      <v-card-text class="py-2 px-6 pb-4">
+        目前看診進度： {{ registrationInfoContent.nowRegistration }}
+      </v-card-text>
+    </v-row>
+  </div>
+</template>
+
+<script lang="ts">
+import { Vue, Component } from "vue-property-decorator";
+
+@Component
+export default class RegistrationInfo extends Vue {
+  registrationInfoContent: Object = {
+    doctorName: "黃俊凱",
+    totleRegistration: 87,
+    nowRegistration: 78
+  };
+}
+</script>
--- src/views/About.vue
+++ src/views/About.vue
@@ -2,15 +2,19 @@
   <v-container class="grey lighten-5">
     <v-row justify="center">
       <v-col md="2">
-        <v-card class="pa-2" tile>
+        <v-card class="pa-2 elevation-12" tile>
           <v-list rounded>
             <v-list-item-group v-model="item" color="primary">
-              <v-list-item v-for="(item, i) in items" :key="i">
+              <v-list-item
+                v-for="(item, i) in items"
+                :key="i"
+                @click="containText.title = item.title"
+              >
                 <v-list-item-icon>
                   <v-icon v-text="item.icon"></v-icon>
                 </v-list-item-icon>
                 <v-list-item-content>
-                  <v-list-item-title v-text="item.text"></v-list-item-title>
+                  <v-list-item-title v-text="item.title"></v-list-item-title>
                 </v-list-item-content>
               </v-list-item>
             </v-list-item-group>
@@ -19,15 +23,17 @@
       </v-col>
 
       <v-col md="6">
-        <v-card class="pa-2" tile>
-          <v-card-title class="headline mb-1">院所介紹</v-card-title>
+        <v-card class="pa-2 elevation-12" tile>
+          <v-card-title class="headline mb-1">{{
+            containText.title
+          }}</v-card-title>
           <p>
-            &emsp;本診所管理系統為一 Web
-            Application，目的為提供診所一個簡易、通用的管理介面，
-            並開源程式碼、開放擴充，使診所能針對各自領域的不同進行擴充，同時也能兼具通用管
-            理的功能。<br /><br />
-            &emsp;系統開發上，前端介面使用 Vue.js 框架技術實做；後端使用
-            Python；資料庫使用 MongoDB 及 FHIR
+            本診所管理系統為一 Web
+            Application，目的為提供診所一個簡易、通用的管理介面，並開源程式碼、開放擴充，使診所能針對各自領域的不同進行擴充，同時也能兼具通用管理的功能。
+          </p>
+          <p>
+            系統開發上，前端介面使用 Vue.js
+            框架技術實做；後端使用Python；資料庫使用 MongoDB 及 FHIR
             Server；資料的查詢（query）及更新使用 GraphQL 進行。
           </p>
         </v-card>
@@ -41,11 +47,15 @@ import { Vue, Component } from "vue-property-decorator";
 
 @Component
 export default class About extends Vue {
+  containText: Object = {
+    title: "院所介紹",
+    icon: String
+  };
   items: Array<Object> = [
-    { text: "院所介紹", icon: "mdi-hospital-building" },
-    { text: "主治項目", icon: "mdi-stethoscope" },
-    { text: "醫師介紹", icon: "mdi-doctor" },
-    { text: "院所位置", icon: "mdi-map-marker" }
+    { title: "院所介紹", icon: "mdi-hospital-building" },
+    { title: "主治項目", icon: "mdi-stethoscope" },
+    { title: "醫師介紹", icon: "mdi-doctor" },
+    { title: "院所位置", icon: "mdi-map-marker" }
   ];
   Introduction: String = "123";
 }
