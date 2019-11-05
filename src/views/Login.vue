2a08fbb91511926cbceaf96947701d500bf7a078
Mino <madness48596@gmail.com>
2019-11-05 23:50:07 +0800

    Implement login & router guard

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,22 +1,30 @@
 <template>
   <v-app id="login">
     <v-content id="login container">
-      <v-container class="fill-height" fluid>
+      <v-container
+        :style="{
+          'background-image':
+            'linear-gradient(0deg, rgba(192,192,217,0.2) 11%, rgba(97,83,87,0.5) 100%), url(' +
+            image +
+            ')'
+        }"
+        class="fill-height"
+        fluid
+      >
         <v-row align="center" justify="center">
           <v-col cols="12" sm="8" md="4">
             <v-card class="elevation-12">
               <v-toolbar color="primary" dark flat>
-                <v-toolbar-title>Login form</v-toolbar-title>
+                <v-toolbar-title>CMS login form</v-toolbar-title>
                 <v-spacer></v-spacer>
               </v-toolbar>
               <v-card-text>
                 <v-form>
                   <v-text-field
                     autofocus
-                    label="Login"
-                    name="login"
+                    label="Email"
+                    name="email"
                     v-model="email"
-                    placeholder="user@example.com"
                     prepend-icon="mdi-account"
                     type="text"
                   ></v-text-field>
@@ -44,13 +52,25 @@
 
 <script lang="ts">
 import { Vue, Component } from "vue-property-decorator";
-import { Mutation } from "vuex-class";
+import { Mutation, State } from "vuex-class";
 
 @Component
 export default class Login extends Vue {
   @Mutation("User/UserLoaded") userLoaded!: Function;
+  @State("image", { namespace: "Dashboard" }) image!: String;
+  @State("user", { namespace: "User" }) user!: Object;
+
   private email: string = "";
   private password: string = "";
+
+  beforeRouteEnter(to: any, from: any, next: (vm: any) => void) {
+    next((vm: any) => {
+      if (vm.user != undefined) {
+        vm.$router.push("/admin");
+      }
+    });
+  }
+
   onLogin() {
     this.axios
       .post(
