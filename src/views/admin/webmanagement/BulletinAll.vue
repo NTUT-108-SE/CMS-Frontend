a9b7cfc157589ab0ace24731048715ede737a7ba
Allen <s9151427@gmail.com>
2019-11-29 20:05:23 +0800

    add webmanagement

---
+++ src/views/admin/webmanagement/BulletinAll.vue
@@ -0,0 +1,71 @@
+<template>
+  <v-container class="grey lighten-5">
+    <v-row justify="center">
+      <v-col md="12">
+        <v-data-table
+          :headers="headers"
+          :items="desserts"
+          sort-by="bulletinNum"
+          class="elevation-1"
+        >
+          <template v-slot:top>
+            <v-toolbar flat color="white">
+              <v-toolbar-title>公告列表</v-toolbar-title>
+            </v-toolbar>
+          </template>
+          <template v-slot:item.action="{ item }">
+            <v-icon class="mr-2" @click="editItem(item)">
+              mdi-account-edit-outline
+            </v-icon>
+            <v-icon class="mr-2" @click="deleteItem(item)">
+              mdi-delete
+            </v-icon>
+            <v-icon class="mr-2" @click="showItem(item)">
+              mdi-file-eye-outline
+            </v-icon>
+          </template>
+        </v-data-table>
+      </v-col>
+    </v-row>
+  </v-container>
+</template>
+
+<script lang="ts">
+import { Vue, Component } from "vue-property-decorator";
+
+@Component
+export default class BulletinAll extends Vue {
+  search: string = "";
+  headers: Object = [
+    {
+      text: "公告號碼",
+      align: "left",
+      value: "bulletinNum"
+    },
+    { text: "公告日期", value: "bulletinDate" },
+    { text: "標題", value: "title" },
+    { text: "點閱率", value: "ctr" },
+    { text: "操作", value: "action", sortable: false }
+  ];
+  desserts: object = [];
+  created() {
+    this.initialize();
+  }
+  initialize() {
+    this.desserts = [
+      {
+        bulletinNum: "0",
+        bulletinDate: "2019-10-20",
+        title: "測試文章",
+        ctr: "1"
+      },
+      {
+        bulletinNum: "1",
+        bulletinDate: "2019-10-20",
+        title: "測試文章2",
+        ctr: "10"
+      }
+    ];
+  }
+}
+</script>
---
+++ src/views/admin/webmanagement/WebDesign.vue
@@ -0,0 +1,149 @@
+<template>
+  <v-container class="fill-height">
+    <v-row justify="center" align="center">
+      <v-col md="6">
+        <v-card class="elevation-12">
+          <v-toolbar dark color="primary">
+            <v-toolbar-title>網頁內容設定</v-toolbar-title>
+          </v-toolbar>
+          <v-card-text>
+            <v-container>
+              <v-form class="py-3">
+                <v-row justify="">
+                  <v-col md="6">
+                    <v-text-field
+                      label="圖片網址1"
+                      prepend-icon="mdi-image"
+                      v-model="imageUrlOne"
+                      :rules="[() => !!imageUrlOne || '必須填入']"
+                      clearable
+                      dense
+                      outlined
+                    ></v-text-field>
+                  </v-col>
+                  <v-col md="6">
+                    <v-text-field
+                      label="圖片網址2"
+                      prepend-icon="mdi-image"
+                      v-model="imageUrlTwo"
+                      :rules="[() => !!imageUrlTwo || '必須填入']"
+                      clearable
+                      dense
+                      outlined
+                    ></v-text-field>
+                  </v-col>
+                </v-row>
+                <v-row justify="">
+                  <v-col md="6">
+                    <v-text-field
+                      label="圖片網址3"
+                      prepend-icon="mdi-image"
+                      v-model="imageUrlThree"
+                      :rules="[() => !!imageUrlThree || '必須填入']"
+                      clearable
+                      dense
+                      outlined
+                    ></v-text-field>
+                  </v-col>
+                  <v-col md="6">
+                    <v-text-field
+                      label="圖片網址4"
+                      prepend-icon="mdi-image"
+                      v-model="imageUrlFour"
+                      :rules="[() => !!imageUrlFour || '必須填入']"
+                      clearable
+                      dense
+                      outlined
+                    ></v-text-field>
+                  </v-col>
+                </v-row>
+                <v-row justify="">
+                  <v-col md="12">
+                    <v-textarea
+                      label="院所介紹"
+                      prepend-icon="mdi-file-document-edit-outline"
+                      v-model="clinicIntroduction"
+                      :rules="[() => !!clinicIntroduction || '必須填入']"
+                      clearable
+                      dense
+                      outlined
+                    ></v-textarea>
+                  </v-col>
+                </v-row>
+                <v-row justify="">
+                  <v-col md="12">
+                    <v-textarea
+                      label="主治項目"
+                      prepend-icon="mdi-file-document-edit-outline"
+                      v-model="indications"
+                      :rules="[() => !!indications || '必須填入']"
+                      clearable
+                      dense
+                      outlined
+                    ></v-textarea>
+                  </v-col>
+                </v-row>
+                <v-row justify="">
+                  <v-col md="12">
+                    <v-textarea
+                      label="醫師介紹"
+                      prepend-icon="mdi-file-document-edit-outline"
+                      v-model="doctorIntroduction"
+                      :rules="[() => !!doctorIntroduction || '必須填入']"
+                      clearable
+                      dense
+                      outlined
+                    ></v-textarea>
+                  </v-col>
+                </v-row>
+                <v-row justify="">
+                  <v-col md="12">
+                    <v-textarea
+                      label="院所位置"
+                      prepend-icon="mdi-file-document-edit-outline"
+                      v-model="clinicLocation"
+                      :rules="[() => !!clinicLocation || '必須填入']"
+                      clearable
+                      dense
+                      outlined
+                    ></v-textarea>
+                  </v-col>
+                </v-row>
+                <v-row justify="center pb-3">
+                  <v-btn class="mx-12" dark color="primary" @click="submit">
+                    <v-icon left>mdi-send-check</v-icon>
+                    設定
+                  </v-btn>
+                  <v-btn class="mx-12" @click="close" dark color="red">
+                    <v-icon left>mdi-close </v-icon>
+                    關閉
+                  </v-btn>
+                  <v-btn class="mx-12" @click="clear" dark color="secondary">
+                    <v-icon left>mdi-autorenew</v-icon>
+                    清除
+                  </v-btn>
+                </v-row>
+              </v-form>
+            </v-container>
+          </v-card-text>
+        </v-card>
+      </v-col>
+    </v-row>
+  </v-container>
+</template>
+
+<script lang="ts">
+import { Vue, Component } from "vue-property-decorator";
+
+@Component
+export default class WebDesign extends Vue {
+  imageUrlOne: string = "";
+  imageUrlTwo: string = "";
+  imageUrlThree: string = "";
+  imageUrlFour: string = "";
+  clinicIntroduction: string = "";
+  indications: string = "";
+  doctorIntroduction: string = "";
+  clinicLocation: string = "";
+}
+</script>
